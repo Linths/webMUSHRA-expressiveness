39c26adfc98bc518e11b370446f5938320fe4d18
Lindsay Kempen <code@linths.com>
2022-02-28 11:18:59 +0100

    Add consent, anonymizes names, MUSHRA subcategories (with neutral and other specification). Fix disabled looping. Add MUSHRA shortcuts left and right.

--- startup.js
+++ startup.js
@@ -77,12 +77,17 @@ function addPagesToPageManager(_pageManager, _pages) {
       var pageConfig = _pages[i];
       if (pageConfig.type == "generic") {
         _pageManager.addPage(new GenericPage(_pageManager, pageConfig));
+      } else if (pageConfig.type == "consent") {
+        _pageManager.addPage(new ConsentPage(_pageManager, pageTemplateRenderer, pageConfig));
       } else if (pageConfig.type == "volume") {
         var volumePage = new VolumePage(_pageManager, audioContext, audioFileLoader, pageConfig, config.bufferSize, errorHandler, config.language);
         _pageManager.addPage(volumePage);
       } else if (pageConfig.type == "mushra") {
         var mushraPage = new MushraPage(_pageManager, audioContext, config.bufferSize, audioFileLoader, session, pageConfig, mushraValidator, errorHandler, config.language);
         _pageManager.addPage(mushraPage);
+      } else if (pageConfig.type == "mushra_subcat") {
+        var mushraSubcatPage = new MushraSubcatPage(_pageManager, pageTemplateRenderer, audioContext, config.bufferSize, audioFileLoader, session, pageConfig, mushraValidator, errorHandler, config.language);
+        _pageManager.addPage(mushraSubcatPage);
       } else if ( pageConfig.type == "spatial"){
         _pageManager.addPage(new SpatialPage(_pageManager, pageConfig, session, audioContext, config.bufferSize, audioFileLoader, errorHandler, config.language));
       } else if (pageConfig.type == "paired_comparison") {
